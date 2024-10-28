784dbe01f102ea7c8ada1743a4ba7c0239d9503c
zebal <zeballosperedodylanamin@gmail.com>
2024-10-28 15:43:14 -0400

    fix: deploy2

--- server/src/controller/audiobookController.js
+++ server/src/controller/audiobookController.js
@@ -11,25 +11,20 @@ const ttsClient = new textToSpeech.TextToSpeechClient();
 const storage = new Storage();
 
 
-
-const credentials = {
-  type: process.env.GOOGLE_TYPE,
-  project_id: process.env.GOOGLE_PROJECT_ID,
-  private_key_id: process.env.GOOGLE_PRIVATE_KEY_ID,
-  private_key: process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, '\n'),
-  client_email: process.env.GOOGLE_CLIENT_EMAIL,
-  client_id: process.env.GOOGLE_CLIENT_ID,
-  auth_uri: process.env.GOOGLE_AUTH_URI,
-  token_uri: process.env.GOOGLE_TOKEN_URI,
-  auth_provider_x509_cert_url: process.env.GOOGLE_AUTH_PROVIDER_CERT_URL,
-  client_x509_cert_url: process.env.GOOGLE_CLIENT_CERT_URL,
-  universe_domain: process.env.GOOGLE_UNIVERSE_DOMAIN
-};
-
 // Usa las credenciales en la configuración del cliente de Google Cloud
 const { GoogleAuth } = require('google-auth-library');
 const auth = new GoogleAuth({
-   credentials,
+  process.env.GOOGLE_TYPE,
+  process.env.GOOGLE_PROJECT_ID,
+  process.env.GOOGLE_PRIVATE_KEY_ID,
+  process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+  process.env.GOOGLE_CLIENT_EMAIL,
+  process.env.GOOGLE_CLIENT_ID,
+  process.env.GOOGLE_AUTH_URI,
+  process.env.GOOGLE_TOKEN_URI,
+  process.env.GOOGLE_AUTH_PROVIDER_CERT_URL,
+  process.env.GOOGLE_CLIENT_CERT_URL,
+  process.env.GOOGLE_UNIVERSE_DOMAIN,
    scopes: ['https://www.googleapis.com/auth/cloud-platform']
 });
 
