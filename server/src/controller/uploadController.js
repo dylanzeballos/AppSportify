f976768c48a0a90654d351328755aaf0ab310e8c
zebal <zeballosperedodylanamin@gmail.com>
2024-10-28 02:14:52 -0400

    fix: server

--- server/src/controller/uploadController.js
+++ server/src/controller/uploadController.js
@@ -1,10 +1,20 @@
 const { Storage } = require('@google-cloud/storage');
-const path = require('path');
+require('dotenv').config();
 
 // Configura las credenciales de Google Cloud
-const credentialsPath = path.join(__dirname, '.../src/google-key.json');
-process.env.GOOGLE_APPLICATION_CREDENTIALS = credentialsPath;
-
+// Configuración de credenciales y bucket de GCS
+const credentials = {
+    type: process.env.GOOGLE_TYPE,
+    project_id: process.env.GOOGLE_PROJECT_ID,
+    private_key_id: process.env.GOOGLE_PRIVATE_KEY_ID,
+    private_key: process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, '\n'), // Reemplaza saltos de línea para formatearlo correctamente
+    client_email: process.env.GOOGLE_CLIENT_EMAIL,
+    client_id: process.env.GOOGLE_CLIENT_ID,
+    auth_uri: process.env.GOOGLE_AUTH_URI,
+    token_uri: process.env.GOOGLE_TOKEN_URI,
+    auth_provider_x509_cert_url: process.env.GOOGLE_AUTH_PROVIDER_CERT_URL,
+    client_x509_cert_url: process.env.GOOGLE_CLIENT_CERT_URL
+  };
 const gcsClient = new Storage(); // Inicializa el cliente de GCS
 
 // Función para subir archivos a Google Cloud Storage
